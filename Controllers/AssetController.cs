a432132a5498d0b81972fa7eacce564fc2c0b738
AthanIsaac <52258815+AthanIsaac@users.noreply.github.com>
2023-02-23 19:21:58 -0800

    Asset CRUD apis (#2)
    Co-authored-by: athanasius_isaac <athanasius_isaac@apple.com>

--- Controllers/AssetController.cs
+++ Controllers/AssetController.cs
@@ -26,5 +26,57 @@ public class AssetController : ControllerBase
             return assets;
         }
     }
-}
 
+
+    [HttpPost(Name = "CreateAsset")]
+    public async Task<IActionResult> Create([FromBody] Asset asset)
+    {
+        using (var connection = _dapperContext.GetConnection())
+        {
+            var sqlStatement = @"
+            INSERT INTO Assets 
+                (TypeId,
+                Location,
+                EnglishName,
+                CopticName)
+            VALUES (@TypeId,
+                @Location,
+                @EnglishName,
+                @CopticName)";
+            await connection.ExecuteAsync(sqlStatement, asset);
+        }
+        return Ok();
+    }
+
+    [HttpPut(Name = "UpdateAsset")]
+    public async Task<IActionResult> Update([FromBody] Asset asset)
+    {
+        _logger.LogInformation("Update: asset=" + asset.AssetId);
+        using (var connection = _dapperContext.GetConnection())
+        {
+            var sqlStatement = @"
+            UPDATE Assets 
+                SET TypeId = @TypeId,
+                Location = @Location,
+                EnglishName = @EnglishName,
+                CopticName = @CopticName
+            WHERE AssetId = @AssetId";
+            await connection.ExecuteAsync(sqlStatement, asset);
+        }
+        return Ok();
+    }
+
+        [HttpDelete("{assetId:int}")]
+    public async Task<IActionResult> Delete(int assetId)
+    {
+        _logger.LogInformation("Delete: assetId=" + assetId);
+        using (var connection = _dapperContext.GetConnection())
+        {
+            var sqlStatement = @"
+            DELETE FROM Assets
+            WHERE AssetId = @assetId";
+            await connection.ExecuteAsync(sqlStatement, new {assetId = assetId});
+        }
+        return Ok();
+    }
+}
