305d2893b362fc05b199ff582d7c3443fc8a5ae9
Verena Girgis <verenagirgis22@gmail.com>
2023-02-28 14:32:58 -0800

    add front-end

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var _policyName = "CorsPolicy";
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddCors(opt =>
     {
@@ -15,12 +15,24 @@ builder.Services.AddCors(opt =>
     });
 var app = builder.Build();
 
-// app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors(_policyName);
 
-app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html");
 
 app.MapFallbackToFile("index.html");
 
