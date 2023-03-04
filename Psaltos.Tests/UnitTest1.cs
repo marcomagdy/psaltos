ae46d4c31dcab518ce3e8b856aee771ac93ccfb5
Nardin Eshak <57229979+nardineshak@users.noreply.github.com>
2023-03-04 12:49:57 -0800

    Added CI Workflow & Attempt at creating test package (#6)
    * updated contributors list in README.md
    
    * Create pullrequestCI.yml
    
    * Update pullrequestCI.yml
    
    * Update pullrequestCI.yml
    
    * Update pullrequestCI.yml
    
    * Attempt at creating test package
    
    * Deleted TestClassAttribute.cs & TestMethodAttribute.cs
    
    * Update pullrequestCI.yml
    
    * Update pullrequestCI.yml
    
    ---------
    
    Co-authored-by: nardineshak <neshak@cs.washington.edu>

---
+++ Psaltos.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Psaltos.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
+
