7b72790b43ac5b876e9731e0129c9ef90f0aed5e
Marco Magdy <marcomag@apple.com>
2023-03-16 09:36:22 -0700

    Fixing cruft that was added with React
    This allows the backend to be used with curl

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var _policyName = "CorsPolicy";
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddCors(opt =>
     {
@@ -15,22 +15,12 @@ builder.Services.AddCors(opt =>
     });
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseCors(_policyName);
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.MapFallbackToFile("index.html");
 
