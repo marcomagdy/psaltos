6791a7d7695d6f300ea66fdf776ed682f7b2aaec
verenagirgis <106639780+verenagirgis@users.noreply.github.com>
2023-03-18 18:04:36 -0700

    bad merge

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,14 +1,12 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
-import FileUploader from "./components/FileUploader";
-import { Home } from "./components/Home";
+import Home  from "./components/Home";
+import  FetchData  from "./components/FetchData";
+import AddData  from "./AddData"
 
 const AppRoutes = [
   {
     index: true,
     element: <Home />
   },
-  {
   {
     path: '/fetch-data',
     element: <FetchData />
